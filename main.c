4ddcd0fdc2705f1c5f6045d5e77d755eb5c90cea
fyog <eric.alex.ross@gmail.com>
2021-02-19 09:03:16 -0700

    Add files via upload
    Fixed the way we were writing the pinValue to the buttons register (before it was just writing over the same location for every button).

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include "initGPIO.h"
 #include "wiringPi.h"
+
 #define GPFSEL0 0
 #define GPFSEL1 1
-#define GPSET0 8
-#define GPCLR0 11
-#define GPLEV0 14
+#define GPSET0 7
+#define GPCLR0 10
+#define GPLEV0 13
 #define LATCH 9
 #define DATA 10
 #define CLOCK 11
@@ -34,39 +36,31 @@ int main()
     
     while (running){
 
-
 		// Increment GPIO ptr
 		unsigned int buttons = 0;
     
 		// Write 1 to CLOCK
-		//gpioPtr = gpioPtr + 28;
 		int pinValue = (gpioPtr[GPSET0] &= (1 << CLOCK)) > 0;
-		//printf("Address: %d\nPin Value: %d\n\n", gpioPtr, pinValue);
     
 		// Write 1 to LATCH
 		pinValue = (gpioPtr[GPSET0] &= (1 << LATCH)) > 0;
-		//printf("Address: %d\nPin Value: %d\n\n", gpioPtr, pinValue);
     
 		// Wait 12 micro seconds
 		delayMicroseconds(12);
     
 		// Write 0 to LATCH
 		pinValue = (gpioPtr[GPCLR0] &= (1 << LATCH)) > 0;
-		//printf("Address: %d\nPin Value: %d\n\n", gpioPtr, pinValue);
+		printf("Address: %d\nPin Value: %d\n\n", gpioPtr, pinValue);
     
 		// Initialize counter
-		int i;
+		int i = 1;
 		
 		// Set bool to true
 		while (i <= 16) {
-                
-			// Counter equals 1
-			i = 1;
-		
+                		
 			// Wait 6 micro seconds
 			delayMicroseconds(6);
 
-    
 			// Write 0 to clock
 			pinValue = (gpioPtr[GPCLR0] &= (1 << CLOCK)) > 0;
 			//printf("Address: %d\nPin Value: %d\n\n", gpioPtr, pinValue);
@@ -79,7 +73,7 @@ int main()
 			//printf("Address: %d\nPin Value: %d\n\n", gpioPtr, pinValue);
     
 			// Set buttons register
-			buttons &= pinValue;
+			buttons |= (pinValue << i);
         
 			// Write 0 to CLOCK
 			pinValue = (gpioPtr[GPCLR0] &= (1 << LATCH)) > 0;
