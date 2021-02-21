444fd71404763b2c26b3fbd07a71b3115a51b056
fyog <eric.alex.ross@gmail.com>
2021-02-21 14:48:49 -0700

    Add files via upload
    fixed the button checking at the end of the loop. also fixed some other areas where we were not reading and writing correctly.

--- main.c
+++ main.c
@@ -59,7 +59,7 @@ int main()
 			// Wait 6 micro seconds
 			delayMicroseconds(6);
 
-			// Write 0 to clock
+			// Write 0 to CLOCK
 			pinValue = (gpioPtr[GPCLR0] &= (1 << CLOCK)) > 0;
     
 			// Wait 6 micro seconds
@@ -71,69 +71,92 @@ int main()
 			// Set buttons register
 			buttons |= (pinValue << i);
         
-			// Write 0 to CLOCK
-			pinValue = (gpioPtr[GPCLR0] &= (1 << LATCH)) > 0;
+			// Write 1 to CLOCK
+			pinValue = (gpioPtr[GPSET0] &= (1 << CLOCK)) > 0;
     
 			// Increment i
 			i += 1;
 		}
 		
+		// Print buttons register
+		i = 0;
+		while (i < 32){
+			pinValue = (buttons >> i) & 1;
+			printf("%d", pinValue);
+			if (i == 31) {
+				printf("\n");
+			}
+			i += 1;
+		}
 		// Check if B is being pressed
-		pinValue = (buttons &= 1);
+		pinValue = (buttons >> 0) & 1
+		;
 		if (pinValue == 1) {
 			printf("B is being pressed...");
 		}
 		
 		// Check if Y is being pressed
-		pinValue = (buttons &= 4);
+		pinValue = (buttons >> 1) & 1;
 		if (pinValue == 1) {
 			printf("Y is being pressed...");
 		}
 		
 		// Check if SELECT is being pressed
-		pinValue = (buttons &= 8);
+		pinValue = (buttons >> 2) & 1;
 		if (pinValue == 1) {
 			printf("SELECT is being pressed...");
 		}
 		
 		// Check if START is being pressed
-		pinValue = (buttons &= 16);
+		pinValue = (buttons >> 3) & 1;
 		if (pinValue == 1) {
 			printf("START is being pressed...");
 		}
 		
 		// Check if UP is being pressed
-		pinValue = (buttons &= 32);
+		pinValue = (buttons >> 4) & 1;
 		if (pinValue == 1) {
 			printf("UP is being pressed...");
 		}
 		
 		// Check if DOWN is being pressed
-		pinValue = (buttons &= 64);
+		pinValue = (buttons >> 5) &  1;
 		if (pinValue == 1) {
 			printf("DOWN is being pressed...");
 		}
 		
 		// Check if LEFT is being pressed
-		pinValue = (buttons &= 128);
+		pinValue = (buttons >> 6) & 1;
 		if (pinValue == 1) {
 			printf("LEFT is being pressed...");
 		}
 		
 		// Check if RIGHT is being pressed
-		pinValue = (buttons &= 256);
+		pinValue = (buttons >> 7) & 1;
 		if (pinValue == 1) {
 			printf("RIGHT is being pressed...");
 		}
 		
+		// Check if A is being pressed
+		pinValue = (buttons >> 8) & 1;
+		if (pinValue == 1) {
+			printf("A is being pressed...");
+		}
+		
+		// Check if X is being pressed
+		pinValue = (buttons >> 9) & 1;
+		if (pinValue == 1) {
+			printf("X is being pressed...");
+		}
+		
 		// Check if L is being pressed
-		pinValue = (buttons &= 512);
+		pinValue = (buttons >> 10) & 1;
 		if (pinValue == 1) {
 			printf("L is being pressed...");
 		}
 		
 		// Check if R is being pressed
-		pinValue = (buttons &= 1028);
+		pinValue = (buttons >> 11) & 1;
 		if (pinValue == 1) {
 			printf("R is being pressed...");
 		}
