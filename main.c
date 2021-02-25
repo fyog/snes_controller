fe12df0a8129431c4a5cb32e2cac9cc76d893430
fyog <eric.alex.ross@gmail.com>
2021-02-25 13:32:41 -0700

    refactored code into several distinct methods
    New methods:
    - initSNES()
    - checkButtons(int arr[])
    - runDriver()
    Also, removed the bool running and replaced it with true in the outer 'running' loop.

--- main.c
+++ main.c
@@ -4,59 +4,125 @@
 #include "initGPIO.h"
 #include "wiringPi.h"
 
+// Constants
 #define GPFSEL0 0
 #define GPFSEL1 1
-
 #define GPSET0 7
 #define GPCLR0 10
 #define GPLEV0 13
-
-#define CLOCK 11
 #define LATCH 9
 #define DATA 10
+#define CLOCK 11
 
+// Macros
 #define INP_GPIO(g) *(gpioPtr+((g)/10)) &= ~(7<<(((g)%10)*3))
 #define OUT_GPIO(g) *(gpioPtr+((g)/10)) |= (1<<(((g)%10)*3))
 #define SET_GPIO_ALT(g,a) *(gpioPtr+(((g)/10))) |= (((a)<=3? (a)+4: (a)==4? 3:2) << (((g)%10)*3))
 
-
-
+// Global variables
 unsigned int *gpioPtr;
 
-int main()
-{
-	// Get GPIO ptr
+// Initializes the GPIO pins so that the SNES controller can communicate with the rPi.
+//
+// argument(s): none
+// returns: nothing
+void initSNES() {
 	
+	// Get GPIO ptr
 	gpioPtr = getGPIOPtr();  
 	printf("pointer address: %p\n", gpioPtr);
 	
-    //set latch to output
-    //gpioPtr[LATCH/10] = (gpioPtr[LATCH/10] & ~(0b111 << ((LATCH%10)*3) | (GPFSEL1 << ((LATCH % 10)*3))));
-    INP_GPIO(LATCH);
-    OUT_GPIO(LATCH);
+	// Set LATCH to output
+	INP_GPIO(LATCH);
+	OUT_GPIO(LATCH);
     
-    //set clock to output
-    //gpioPtr[CLOCK/10] = (gpioPtr[CLOCK/10] & ~(0b111 << ((CLOCK%10)*3) | (GPFSEL1 << ((CLOCK % 10)*3))));
-    INP_GPIO(CLOCK);
-    OUT_GPIO(CLOCK);
+	// Set CLOCK to output
+	INP_GPIO(CLOCK);
+	OUT_GPIO(CLOCK);
     
-    //set data to input
-    //gpioPtr[DATA/10] = (gpioPtr[DATA/10] & ~(0b111 << ((DATA%10)*3) | (GPFSEL0 << ((DATA % 10)*3))));
-    INP_GPIO(DATA);
-    
-    
-    // Initialize running bool
-    bool running = true;
-    //unsigned int buttons = 0;
-    //static int buttons_arr[32];
-    
-    while (running){
+	// Set DATA to input
+	INP_GPIO(DATA);
+}
 
-		// Increment GPIO ptr
-		//unsigned int buttons;
-        int pinValue;
-        static int buttons_arr[20];
-    
+// Checks to see if any of the SNES controller buttons are being pressed.
+//
+// argument(s): array of integers
+// returns: nothing
+void checkButtons(int buttons_arr[]) {
+	
+	// Check if B is being pressed
+	if (buttons_arr[0] == 0) {
+		printf("B is being pressed...\n");
+	}
+		
+	// Check if Y is being pressed
+	if (buttons_arr[1] == 0) {
+		printf("Y is being pressed...\n");
+	}        
+		
+	// Check if SELECT is being pressed
+	if (buttons_arr[2] == 0) {
+		printf("SELECT is being pressed...\n");
+	}
+	
+	// Check if START is being pressed
+	if (buttons_arr[3] == 0) {
+		printf("START is being pressed...\n");
+	}
+	
+	// Check if UP is being pressed
+	if (buttons_arr[4] == 0) {
+		printf("UP is being pressed...\n");
+	}
+	
+	// Check if DOWN is being pressed
+	if (buttons_arr[5] == 0) {
+		printf("DOWN is being pressed...\n");
+	}
+	
+	// Check if LEFT is being pressed
+	if (buttons_arr[6] == 0) {
+		printf("LEFT is being pressed...\n");
+	}
+	
+	// Check if RIGHT is being pressed
+	if (buttons_arr[7] == 0) {
+		printf("RIGHT is being pressed...\n");
+	}
+	
+	// Check if A is being pressed
+	if (buttons_arr[8] == 0) {
+		printf("A is being pressed...\n");
+	}
+	
+	// Check if X is being pressed
+	if (buttons_arr[9] == 0) {
+		printf("X is being pressed...\n");
+	}
+		
+	// Check if L is being pressed
+	if (buttons_arr[10] == 0) {
+		printf("L is being pressed...\n");
+	}
+		
+	// Check if R is being pressed
+	if (buttons_arr[11] == 0) {
+		printf("R is being pressed...\n");
+	}
+	system("clear");
+}
+
+// Starts execution of the SNES controller driver
+//
+// argument(s): none
+// returns: nothing
+void runDriver() {
+	
+	// Running loop
+	while (true){
+
+		// Initialize array that will be used to track button presses
+		int buttons_arr[20];
     
 		// Write 1 to LATCH
 		gpioPtr[GPSET0] = (1 << LATCH);
@@ -67,10 +133,8 @@ int main()
 		// Write 0 to LATCH
 		gpioPtr[GPCLR0] = (1 << LATCH);
         
-        
-        
-        // Write 1 to CLOCK
-        gpioPtr[GPSET0] = 1 << CLOCK;
+		// Write 1 to CLOCK
+		gpioPtr[GPSET0] = 1 << CLOCK;
 	
 		// Initialize counter
 		int i = 1;
@@ -81,122 +145,37 @@ int main()
 			// Wait 6 micro seconds
 			delayMicroseconds(6);
 
-			// Write 0 to clock
+			// Write 0 to CLOCK
 			gpioPtr[GPCLR0] = 1 << CLOCK;
     
 			// Wait 6 micro seconds
 			delayMicroseconds(6);
     
 			// Read bit i
-			pinValue = (gpioPtr[GPLEV0] >> DATA) & 1;
-			//printf("pinvalue: %d i = %d\n", pinValue, i);
-    
+			int pinValue = (gpioPtr[GPLEV0] >> DATA) & 1;    
         
-			// Write 0 to CLOCK
+			// Write 1 to CLOCK
 			gpioPtr[GPSET0] = (1 << CLOCK);
             
-            // Set buttons register
-			//buttons |= (pinValue << i);
-            buttons_arr[i-1] = pinValue;
+			// Set buttons_arr
+			buttons_arr[i-1] = pinValue;
     
 			// Increment i
 			i += 1;
 		}
-        
-/*
-        for(int x = 0; x < 20; x++){
-            printf("%d", buttons_arr[x]);
-
-        }
-        printf("\n");
-*/
-		//printf("buttons reg: %d\n", buttons);
-		
-		// Print buttons register
-        /*
-		i = 1;
-		while (i <= 32){
-			pinValue = (buttons >> i) & 1;
-			printf("%d", pinValue);
-			i += 1;
-		}
-		printf("\n");
-        */
-
-        delay(10);
-		// Check if B is being pressed
-		if (buttons_arr[0] == 0) {
-			printf("B is being pressed...\n");
-		}
-		
-		// Check if Y is being pressed
-		if (buttons_arr[1] == 0) {
-			printf("Y is being pressed...\n");
-		}
-        //will put in the rest of the code to detect button once clock is fixed
-        
-		
-		// Check if SELECT is being pressed
-		//pinValue = (buttons >> 3) & 1;
-		if (buttons_arr[2] == 0) {
-			printf("SELECT is being pressed...\n");
-		}
-		
-		// Check if START is being pressed
-		//pinValue = (buttons >> 4) & 1;
-		if (buttons_arr[3] == 0) {
-			printf("START is being pressed...\n");
-		}
-		
-		// Check if UP is being pressed
-		//pinValue = (buttons >> 5) & 1;
-		if (buttons_arr[4] == 0) {
-			printf("UP is being pressed...\n");
-		}
-		
-		// Check if DOWN is being pressed
-		//pinValue = (buttons >> 6) &  1;
-		if (buttons_arr[5] == 0) {
-			printf("DOWN is being pressed...\n");
-		}
-		
-		// Check if LEFT is being pressed
-		//pinValue = (buttons >> 7) & 1;
-		if (buttons_arr[6] == 0) {
-			printf("LEFT is being pressed...\n");
-		}
-		
-		// Check if RIGHT is being pressed
-		//pinValue = (buttons >> 8) & 1;
-		if (buttons_arr[7] == 0) {
-			printf("RIGHT is being pressed...\n");
-		}
-		
-		// Check if A is being pressed
-		//pinValue = (buttons >> 9) & 1;
-		if (buttons_arr[8] == 0) {
-			printf("A is being pressed...\n");
-		}
-		
-		// Check if X is being pressed
-		//pinValue = (buttons >> 10) & 1;
-		if (buttons_arr[9] == 0) {
-			printf("X is being pressed...\n");
-		}
-		
-		// Check if L is being pressed
-		//pinValue = (buttons >> 11) & 1;
-		if (buttons_arr[10] == 0) {
-			printf("L is being pressed...\n");
-		}
-		
-		// Check if R is being pressed
-		//pinValue = (buttons >> 12) & 1;
-		if (buttons_arr[11] == 0) {
-			printf("R is being pressed...\n");
-		}
-        
-		
-    }
+		checkButtons(buttons_arr);
+	}
 }
-		
+
+// Main method.
+//
+// argument(s): none
+// returns: nothing
+int main() {
+	
+	// Initialize pins
+	initSNES();
+	
+	// Run the driver
+	runDriver();
+}
